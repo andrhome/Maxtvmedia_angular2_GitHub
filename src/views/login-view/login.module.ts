87df28abb8afb66315b36a6cd5155fc899c23f0f
andrhome <homandrej@gmail.com>
2017-05-10 14:35:49 +0300

    Created login template

--- src/views/login-view/login.module.ts
+++ src/views/login-view/login.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule }   from '@angular/forms';
 import { LogInComponent } from "./login.component";
 
 @NgModule({
-    imports: [BrowserModule],
+    imports: [BrowserModule, FormsModule],
     declarations: [LogInComponent]
 })
 export class LogInModule {}
